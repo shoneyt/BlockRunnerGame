b676cc835b594c12172a1efc562c5866fa2d0669
Kaish <nebulekais@gmail.com>
2017-08-28 13:35:30 +0200

    Dodati novi fileovi za kod igre
    Dodate nove skripte koje su ubacene u updateu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+	public void StertGame(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+	public void QuitGame(){
+		Application.Quit ();
+	}
+
+}
